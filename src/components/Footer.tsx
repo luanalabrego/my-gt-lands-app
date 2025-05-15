0fe655da9d1e30e9f5951fd50634c392e7b8ed66
Luana <luanasomogyi@gmail.com>
2025-05-15 18:38:58 -0300

    Usar variáveis de ambiente para Google Sheets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-black text-white text-center py-2 text-sm">
+    <footer className="w-full bg-black text-white text-center py-2 text-sm">
       Criado por Labrego Soluções Digitais
     </footer>
   )
