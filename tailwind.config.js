9ff95f2a770454713e96d0281fe9f205e6b659f7
Luana <luanasomogyi@gmail.com>
2025-05-19 11:19:12 -0300

    sua mensagem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    // as duas pastas onde vivem seus JSX/TSX
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+
+    // se eventualmente usar css/tsx em outro lugar
+    // './src/styles/**/*.{css,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
