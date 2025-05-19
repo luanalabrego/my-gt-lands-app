714fd5e55f6bddd5e07dfd36a2436638d43e434c
Luana <luanasomogyi@gmail.com>
2025-05-19 11:40:40 -0300

    sua mensagem

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 // postcss.config.js
 module.exports = {
-    plugins: {
-      // Plugin do Tailwind em pacote separado
-      '@tailwindcss/postcss': {},
-      // Autoprefixer continua normal
-      autoprefixer: {},
-    },
-  }
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
