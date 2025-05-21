904e689dc2e00c11cf8adebb61d2afd25c451dbd
Luana <luanasomogyi@gmail.com>
2025-05-21 17:36:08 -0300

    sua mensagem

--- src/app/propriedades/[numero]/layout.tsx
+++ src/app/propriedades/[numero]/layout.tsx
@@ -5,6 +5,8 @@ import { ReactNode } from 'react'
 
 export default function PropertyLayout({ children }: { children: ReactNode }) {
   return (
-    <>{children}</>
+    <div className="max-w-xl mx-auto bg-[#2C2C2C] p-6 rounded-2xl shadow-lg mt-8 text-white">
+      {children}
+    </div>
   )
 }
