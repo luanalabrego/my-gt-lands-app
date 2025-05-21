176a381d0ad2b31da8c2fffea7b205cec1d29451
Luana <luanasomogyi@gmail.com>
2025-05-21 17:35:42 -0300

    sua mensagem

---
+++ src/app/propriedades/[numero]/layout.tsx
@@ -0,0 +1,10 @@
+// src/app/propriedades/[numero]/layout.tsx
+'use client'
+
+import { ReactNode } from 'react'
+
+export default function PropertyLayout({ children }: { children: ReactNode }) {
+  return (
+    <>{children}</>
+  )
+}
